08207b6d517b69692968787b39ff09a689b47688
LIULI <1154882034@qq.com>
2018-04-11 16:51:46 +0800

    .

---
+++ js.js
@@ -0,0 +1,12 @@
+function wel(){
+	alert("欢迎您提出宝贵的意见。谢谢！");
+	}
+	
+function text(){
+	 var a1=document.getElementById("area");
+	 a1.value='';
+	}
+	function text2(){
+	 var a1=document.getElementById("area");
+	 a1.value="请输入您的意见或建议";
+    }	
